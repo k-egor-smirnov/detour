b0597e10863f573fa80df23aaad826208334a828
k-egor-smirnov <esmirnov.xyz@gmail.com>
2016-11-24 17:18:43 +0500

    1.2.3

--- src/ru/alphach1337/detour/Main.java
+++ src/ru/alphach1337/detour/Main.java
@@ -1,17 +1,21 @@
 package ru.alphach1337.detour;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Main extends JavaPlugin{
+
+public class Main extends JavaPlugin implements Listener {
     HashMap<Location, String> loc2 = new HashMap<Location, String>();
     ArrayList<Location> loc = new ArrayList<Location>();
     ArrayList<String> ignorePlayers = new ArrayList<String>();
@@ -19,90 +23,117 @@ public class Main extends JavaPlugin{
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
 
     }
-    public boolean onCommand(final CommandSender sender, final Command cmd,final String commandLabel, final String[] args) {
-        if (!(sender instanceof Player)){
-            return false; 
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (isDetour) {
+            event.getPlayer().sendMessage(settings.Started1);
+            event.getPlayer().sendMessage(settings.Started2);
         }
-        if (args.length == 0){
+    }
+
+    public boolean onCommand(final CommandSender sender, final Command cmd, final String commandLabel, final String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Только игроки могут использовать эту команду!");
+        }
+        if (args.length == 0) {
             return false;
         }
+
         Player p = (Player) sender;
         if (args[0].equalsIgnoreCase("join")) {
-            if (!isDetour){
-                return false;
+            if (!isDetour) {
+                p.sendMessage(settings.notStarted);
+                return true;
             }
             for (Location l2 : loc2.keySet()) {
                 if (p.getName().equals(loc2.get(l2))) {
-                    return false;
+                    p.sendMessage(settings.alreadyInTheList);
+                    return true;
                 }
             }
-            if (ignorePlayers.contains(p.getName())){
-                return false;
+            if (ignorePlayers.contains(p.getName())) {
+                p.sendMessage(settings.alreadyInTheList);
+                return true;
             }
+            p.sendMessage(settings.addedToList);
             Location l = p.getLocation().clone();
             loc2.put(l.clone(), p.getName());
             loc.add(l.clone());
             return true;
         }
         if (args[0].equalsIgnoreCase("next")) {
-            if (!p.isOp()){
-                return false;
+            if (!p.isOp()) {
+                p.sendMessage(settings.hasNoPermission);
+                return true;
+            }
+            if (!isDetour) {
+                p.sendMessage(settings.notStarted);
+                return true;
             }
             if (loc.isEmpty()) {
-                return false;
+                p.sendMessage(settings.notJoined);
+                return true;
             }
             p.teleport(loc.get(0));
             for (Location st : loc2.keySet()) {
-                if (loc.get(0).equals(st)){
+                if (loc.get(0).equals(st)) {
                     String pl = loc2.get(st);
                     loc.remove(0);
                     loc2.remove(st);
                     ignorePlayers.add(pl);
-                    p.sendMessage("Добро пожаловать к игроку " + pl);
                     try {
                         Player p2 = Bukkit.getPlayer(pl);
-                        p.sendMessage("Игрок " + p2.getName() + " онлайн");
+                        p.sendMessage(ChatColor.GREEN + "Добро пожаловать к игроку " + ChatColor.BLUE + p2.getName());
+                        p.sendMessage(ChatColor.YELLOW + "Осталось: " + ChatColor.DARK_PURPLE + loc.size());
                     } catch (Exception e) {
-                        p.sendMessage("Игрок " + pl + " оффлайн");
+                        p.sendMessage(ChatColor.GREEN + "Добро пожаловать к игроку " + ChatColor.BLUE + pl + ChatColor.RED + " (оффлайн)");
+                        p.sendMessage(ChatColor.YELLOW + "Осталось: " + ChatColor.DARK_PURPLE + loc.size());
                     }
                     return true;
                 }
             }
-            return true;
         }
         if (args[0].equalsIgnoreCase("start")) {
-            if (!p.isOp()){
-                return false;
+            if (!p.isOp()) {
+                p.sendMessage(settings.hasNoPermission);
+                return true;
             }
-            if (isDetour){
-                return false;
+            if (isDetour) {
+                p.sendMessage(settings.alreadyStarted);
+                return true;
             }
+            Bukkit.broadcastMessage(settings.Started1);
+            Bukkit.broadcastMessage(settings.Started2);
             isDetour = true;
-            //send msg;
             return true;
         }
         if (args[0].equalsIgnoreCase("stop")) {
-            if (!p.isOp()){
-                return false;
+            if (!p.isOp()) {
+                p.sendMessage(settings.hasNoPermission);
+                return true;
             }
             if (!isDetour) {
-                return false;
+                p.sendMessage(settings.notStarted);
+                return true;
             }
             isDetour = false;
             loc.clear();
             loc2.clear();
             ignorePlayers.clear();
-            //send msg;
+            Bukkit.broadcastMessage(settings.stopDetour);
             return true;
-        }
-        return false;
 
+        }
+
+        return true;
     }
 
 }
--- src/ru/alphach1337/detour/settings.java
+++ src/ru/alphach1337/detour/settings.java
@@ -3,22 +3,14 @@ package ru.alphach1337.detour;
 import org.bukkit.ChatColor;
 
 public class settings {
-	public static String hasNoPermission = ChatColor.RED + "У вас нет прав на использование данной команды";
-	public static String notStarted = ChatColor.RED + "Обход еще не начался!";
-	public static String stopDetour = ChatColor.RED + "Обход закончен!";
-	public static String Started1 = ChatColor.GREEN + "Обход начался!";
-	public static String Started2 = ChatColor.YELLOW + "Команда " + ChatColor.LIGHT_PURPLE + "/join" + ChatColor.YELLOW
-			+ ", чтобы присоединиться";
-	public static String alreadyInTheList = ChatColor.RED + "Ты уже есть в списке!";
-	public static String addedToList = ChatColor.GREEN + "Ты добавлен в обход! Жди своей очереди.";
-	public static String noPeoples = ChatColor.RED + "Никто еще не присоединился к обходу!";
-	public static String savepos1 = ChatColor.GREEN
-			+ "Твоя позиция будет сохранена!";
-	public static String savepos2 = ChatColor.GREEN
-			+ "Теперь ты можешь выйти.";
-	public static String plsUseNext1 = ChatColor.RED + "Вы еще не перешли к следующему игроку!";
-	public static String plsUseNext2 = ChatColor.RED + "Используйте команду " + ChatColor.LIGHT_PURPLE + "/next";
-	public static String alreadySaved = ChatColor.RED + "Ты уже сохранил свою позицию";
-	public static String notJoined = ChatColor.RED + "Ты еще не присоединился к обходу!";
-
+    public static String hasNoPermission = ChatColor.RED + "РЈ РІР°СЃ РЅРµС‚ РїСЂР°РІ РґР»СЏ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ РґР°РЅРЅРѕР№ РєРѕРјР°РЅРґС‹";
+    public static String notStarted = ChatColor.RED + "РћР±С…РѕРґ РµС‰Рµ РЅРµ РЅР°С‡Р°Р»СЃСЏ!";
+    public static String stopDetour = ChatColor.RED + "РћР±С…РѕРґ Р·Р°РєРѕРЅС‡РµРЅ";
+    public static String Started1 = ChatColor.GREEN + "РћР±С…РѕРґ РЅР°С‡Р°Р»СЃСЏ";
+    public static String Started2 = ChatColor.YELLOW + "РџРёС€Рё РєРѕРјР°РЅРґСѓ " + ChatColor.LIGHT_PURPLE + "/detour join" + ChatColor.YELLOW
+            + ", С‡С‚РѕР±С‹ РїСЂРёСЃРѕРµРґРёРЅРёС‚СЊСЃСЏ";
+    public static String alreadyInTheList = ChatColor.RED + "РўС‹ СѓР¶Рµ СѓС‡Р°СЃС‚РІСѓРµС€СЊ РІ РѕР±С…РѕРґРµ!";
+    public static String addedToList = ChatColor.GREEN + "РўРµРїРµСЂСЊ С‚С‹ СѓС‡Р°СЃС‚РІСѓРµС€СЊ РІ РѕР±С…РѕРґРµ!";
+    public static String notJoined = ChatColor.RED + "Р’ РѕС‡РµСЂРµРґРё РЅРµС‚ РёРіСЂРѕРєРѕРІ!";
+    public static String alreadyStarted = ChatColor.RED + "РћР±С…РѕРґ СѓР¶Рµ РЅР°С‡Р°Р»СЃСЏ!";
 }
