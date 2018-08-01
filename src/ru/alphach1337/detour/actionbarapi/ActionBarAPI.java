309b50a27f73105006d83561b5a5273747bee321
OgyreZ1 <ogyrezmail@gmail.com>
2018-08-01 17:38:40 +0300

    style fix

--- src/ru/alphach1337/detour/actionbarapi/ActionBarAPI.java
+++ src/ru/alphach1337/detour/actionbarapi/ActionBarAPI.java
@@ -8,7 +8,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
